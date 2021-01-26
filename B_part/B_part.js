82d2b90a201fbb542ee93994dce175afed31596c
Sondinh <dnson20022001@gmail.com>
2021-01-26 22:53:42 +0700

    push code

--- B_part/B_part.js
+++ B_part/B_part.js
@@ -1,18 +1,51 @@
+let quizContainer = document.getElementById("quiz-container");
+
 async function fetchData(url) {
-  let response = await fetch(url);
-  let data = await response.json();
-  //   console.log(data);
-  return data;
+    let response = await fetch(url);
+    let data = await response.json();
+    //   console.log(data);
+    return data;
 }
 
-let quizz;
+async function createQuizzes() {
+    let currentData = await fetchData(
+        "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple"
+    );
 
-async function getData() {
-  let data = await fetchData(
-    "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple"
-  );
+    console.log(currentData);
 
-  quizz = data;
+    for (let i = 0; i < currentData.results.length; i++) {
+        let currentQuestion = currentData.results[i].question;
+        let currentQuizz = [
+            currentData.results[i].correct_answer,
+            ...currentData.results[i].incorrect_answers,
+        ];
+        console.log(currentQuizz);
+
+        quizContainer.innerHTML += /*html */ `
+          <h3 class="question question${i}">${currentQuestion}</h3>
+          <div class="answers-container">
+          <div class="answers">
+            <input type="radio" id="question${i}-answer1" name="question${i}-answers"/>
+            <label for="question${i}-answer1">${currentQuizz[0]}</label>
+          </div>
+          <div class="answers">
+            <input type="radio" id="question${i}-answer2" name="question${i}-answers"/>
+            <label for="question${i}-answer1">${currentQuizz[1]}</label>
+          </div>
+          <div class="answers">
+            <input type="radio" id="question${i}-answer3" name="question${i}-answers"/>
+            <label for="question${i}-answer1">${currentQuizz[2]}</label>
+          </div>
+          <div class="answers">
+            <input type="radio" id="question${i}-answer4" name="question${i}-answers"/>
+            <label for="question${i}-answer1">${currentQuizz[3]}</label>
+          </div>
+            
+          </div>
+          
+        `;
+    }
 }
 
-console.log(quizz);
+createQuizzes();
